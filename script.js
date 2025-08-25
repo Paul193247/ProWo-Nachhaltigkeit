e2628906da8c7adab3285ff637e1c17322e0607a
Paul193247 <121023613+Paul193247@users.noreply.github.com>
2025-08-25 07:45:01 +0200

    ka

--- script.js
+++ script.js
@@ -9,4 +9,16 @@ import {
   Balance,
 } from "./library.js";
 
-const grid = new Grid([]);
+const balance = new Balance(1000);
+
+class Park extends Cell {
+  constructor() {
+    super("bild.jpg", "Park", 300);
+  }
+}
+
+const htmlstuff = HTMLStuff();
+
+htmlstuff.add("h1", "Spiel");
+
+const grid = new Grid([Park], balance, 10, 10, 600, 600);
